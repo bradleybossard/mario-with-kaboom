6380bc346fca9187b57b292e2ba48bf1d70aa96e
Bradley Bossard <bradleybossard@gmail.com>
2021-08-23 12:22:30 -0700

    add first working map definition

--- game.js
+++ game.js
@@ -20,17 +20,30 @@ loadSprite('pipe-top-right', 'hj2GK4n.png')
 loadSprite('pipe-bottom-left', 'c1cYSbt.png')
 loadSprite('pipe-bottom-right', 'nqQ79eI.png')
 
-/*
-loadSprite('blue-block', 'fVscIbn.png')
-loadSprite('blue-brick', '3e5YRQd.png')
-loadSprite('blue-steel', 'gqVoI2b.png')
-loadSprite('blue-evil-shroom', 'SvV4ueD.png')
-loadSprite('blue-surprise', 'RMqCc1G.png')
-*/
-
 scene("game", () => {
   // add three layers with obj as default
   layers(['bg', 'obj', 'ui'], 'obj')
-})
 
-start("game")
+	const map = [
+		'                                      ',
+		'                                      ',
+		'                                      ',
+		'                                      ',
+		'                                      ',
+		'     %   =*=%=                        ',
+		'                                      ',
+		'                            -+        ',
+		'                    ^   ^   ()        ',
+		'==============================   =====',
+		];
+
+	const levelCfg = {
+		width: 20,
+		height: 20,
+		'=': [sprite('block'), solid()],
+		};
+
+  const gameLevel = addLevel(map, levelCfg);
+});
+
+start("game");
